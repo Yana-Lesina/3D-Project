f0246785b5c858e9ca3ddfed3f0e24be9e9800ad
Yana-Lesina <yanalesina3@gmail.com>
2021-11-15 11:32:17 +0300

    L19: h/w

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import timer from './modules/timer'
 
-timer('15 december 2021');
+timer('1 december 2021 9:30:00');// 15 november 2021 11:27:00
--- src/modules/timer.js
+++ src/modules/timer.js
@@ -3,11 +3,14 @@ const timer = (deadline) => {
   const timerMinutes = document.getElementById('timer-minutes');
   const timerSeconds = document.getElementById('timer-seconds');
 
+  let refresh //интервальное обновление времени
+
+  const checkForZero = (num) => { return num < 10 ? num = '0' + num : num }
+
   const getTimeRemaining = () => {
     let dateStop = new Date(deadline).getTime()
     let dateNow = new Date().getTime()
     let timeRemaining = (dateStop - dateNow) / 1000;
-    //let days = Math.floor(timeRemaining / 60 / 60 / 24)
     let hours = Math.floor(timeRemaining / 60 / 60)
     let minutes = Math.floor((timeRemaining / 60) % 60)
     let seconds = Math.floor(timeRemaining % 60)
@@ -16,18 +19,25 @@ const timer = (deadline) => {
   }
 
   const updateClock = function() {
+    console.log('updateClock!');
+    
     let getTime = getTimeRemaining()
-    timerHours.textContent = getTime.hours;
-    timerMinutes.textContent = getTime.minutes;
-    timerSeconds.textContent = getTime.seconds;
 
-    if(getTime.timeRemaining > 0) setTimeout(updateClock, 1000)
+    if(getTime.timeRemaining > 0) {
+      
+      timerHours.textContent = checkForZero(getTime.hours);
+      timerMinutes.textContent = checkForZero(getTime.minutes);
+      timerSeconds.textContent = checkForZero(getTime.seconds);
+
+    } else {
+      timerHours.textContent = '00';
+      timerMinutes.textContent = '00';
+      timerSeconds.textContent = '00';
+      clearInterval(refresh)
+    }
   }
-  updateClock();
-
-
-  //countTimer('15 december 2021');
-  //setInterval(countTimer, 1000, '15 december 2021')
+  
+  refresh = setInterval(updateClock, 1000);
 }
 
 export default timer
