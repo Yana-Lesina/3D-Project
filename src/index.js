8fa1115e9bedeccc8bc1ac26f7b2dd39341b56e3
Yana-Lesina <yanalesina3@gmail.com>
2021-11-17 19:37:51 +0300

    L20 h/w: just modal movement, no archery effect & mobiles exception

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import timer from './modules/timer'
+import menu from './modules/menu'
+import modal from './modules/modal'
 
-timer('15 december 2021');
+timer('1 december 2021 9:30:00');// 15 november 2021 11:27:00
+menu()
+modal()
--- src/modules/two.js
+++
@@ -1,6 +0,0 @@
-const func = () => {
-  const msg = "This's funcTwo!";
-  console.log(msg);
-}
-
-export default func
