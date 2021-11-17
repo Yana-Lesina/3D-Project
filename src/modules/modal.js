5f3d7f836bd2e192a3ec0974781fde5cdf51dc6f
Yana-Lesina <yanalesina3@gmail.com>
2021-11-17 23:55:02 +0300

    L20: all h/w

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -5,17 +5,16 @@ const modal = () => {
 
   const popubContent = modal.querySelector('.popup-content')
 
-  // Math.round(popubContent.getBoundingClientRect().height))
+  let inter1 //для запуска/остановки requestAnimationFrame
+  let num = 0 // изменяет  modal.style.top, т.е. выдвигает окно на экран
+  let progress //значение прогресса анимации и её отрисовка
 
-  let inter1;
-  let inter2;
-  let num = 0;
-
-  const moveModalContent = () => {
+  //анимация появления модального окна
+  const addModalContent = () => {
 
     if(num <= 100 ) {
-      num += 10
-      inter1 = requestAnimationFrame(moveModalContent)
+      num += 7
+      inter1 = requestAnimationFrame(addModalContent)
       popubContent.style.top = `${num}px`
 
     } else if (num > 100 ) {
@@ -23,8 +22,9 @@ const modal = () => {
     }
   }
 
+  //простой вариант исчезновения модального окна
+  //let inter2;
   // const removeModalContent = () => {
-    
   //   if(num <= modal.clientHeight) {
   //     num += 20
   //     inter2 = requestAnimationFrame(removeModalContent)
@@ -37,49 +37,52 @@ const modal = () => {
   //   }
   // }
 
-
+  //анимация исчезновения модального окна
   const animate = ({duration, draw, timing}) => {
     let start = performance.now();
 
     requestAnimationFrame(function animate(time) {
       let timeFraction = (time - start) / duration;
-      if (timeFraction > 1) timeFraction = 1;
 
-      let progress = timing(timeFraction)
+      progress = timing(1.5, timeFraction)
 
       draw(progress);
 
       if (timeFraction < 1) {
         requestAnimationFrame(animate);
+      } else {
+        modal.style.display = 'none'
+        num = 0
       }
-
     });
   }
 
   btns.forEach(btn => {
     btn.addEventListener('click', () => {
-      modal.style.display = 'block'
-      moveModalContent()
+      modal.style.display = 'block'    
       
+      if(document.body.clientWidth > 780) addModalContent()
     })
   })
 
-  closeBtn.addEventListener('click', () => {
-      //removeModalContent()
-      //modal.style.display = 'none'
-      //if(num <= modal.clientHeight) {
-        animate({
-          duration: 1000,
-          timing: function back(x, timeFraction) {
-            return Math.pow(timeFraction, 2) * ((x + 1) * timeFraction - x)
-          },
-          draw: function () {
-            popubContent.style.bottom = progress * 500 + 'px'
-          }
-        })
-      //}
 
-      
+  closeBtn.addEventListener('click', () => {
+    //removeModalContent()
+    
+    if(document.body.clientWidth > 780) {
+      animate({
+        duration: 1000,
+        timing: function back(x, timeFraction) {
+          return Math.pow(timeFraction, 2) * ((x + 1) * timeFraction - x)
+        },
+        draw: function () {
+          popubContent.style.top = num + progress * 550 + 'px'
+        }
+      }) 
+
+    } else {
+      modal.style.display = 'none' 
+    }
   })
 }
 
